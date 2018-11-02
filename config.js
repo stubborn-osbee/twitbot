f1f566ad11b89d4ff2319d6ab752768ac1f98a19
unknown <osbornebm406@yahoo.co.uk>
2018-11-02 08:59:37 +0000

    Adding Flesh

---
+++ config.js
@@ -0,0 +1,8 @@
+module.export = {
+
+    consumer_key: 'OtTVHaOOplbB8eVXZdbUJm6z6',
+    consumer_secret: 'PtZyVXU7lkHNlpDFW851OPuX06MKSZgExW7XAQUTdnrm1SWBVC',
+    access_token_key: '1058150296436858880-gpwFun4ehuiLpBWyhkLAXKxoE8WFkb',
+    access_token_secret: 'z2NtDPPuNZgTA7OeMm0PgI3HjoqrkuEnwbSEXlikWNdRR'
+  
+}
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+//Import twitter package using node
 var Twitter = require('twitter');
 
-var client = new Twitter({
-  consumer_key: 'OtTVHaOOplbB8eVXZdbUJm6z6',
-  consumer_secret: 'PtZyVXU7lkHNlpDFW851OPuX06MKSZgExW7XAQUTdnrm1SWBVC',
-  access_token_key: '1058150296436858880-gpwFun4ehuiLpBWyhkLAXKxoE8WFkb',
-  access_token_secret: 'z2NtDPPuNZgTA7OeMm0PgI3HjoqrkuEnwbSEXlikWNdRR'
-});
+//Import config module
+var config =require('./config');
+
+//Autheticating user
+var client = new Twitter(config);
 
 client.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response) {
     if(error) throw error;
